0ac7a4dab26e8c87c1f7f1f6b6eb788dc9e85f84
samovar732 <MaxPlay024@gmail.com>
2023-05-22 01:21:16 +0300

    final version2(Debug = False)

--- MarkingSystem/backend/MarkingSystem/settings.py
+++ MarkingSystem/backend/MarkingSystem/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ylleuia(8nb4!+5d!a!^(642s=xuk2r#gv7r)t^l-tq7@9y3vw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
